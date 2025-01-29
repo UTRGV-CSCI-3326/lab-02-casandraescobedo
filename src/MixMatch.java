0ba6d6bc19a85b5e485126d2a63695158d17b6e7
casandraescobedo <126704586+casandraescobedo@users.noreply.github.com>
2025-01-29 21:42:53 +0000

    Completed the lab.

---
+++ src/MixMatch.java
@@ -0,0 +1,29 @@
+public class MixMatch{
+
+    public static void main(String [] args){
+        String x = "Hello World";
+        System.out.println(x + " <- String which stores letters or sentences");
+
+        char y = '?';
+        System.out.println(y + " <- A character holds a single letter, symbol or number");
+
+        int z = 16;
+        System.out.println(z + " <- An integer holds a single number no decimals");
+
+        float c = 2.00f;
+        System.out.println(c + " <- A float is a fractional value");
+
+        double h = 7.0;
+        System.out.println(h + " <- Double, another data type that stores a fractional value");
+
+        boolean t = true;
+        System.out.println(t + " <- Boolean, a data type that can hold true or false");
+
+        long n = 8999999;
+        System.out.println(n + " <- Long, a data type that stores very large whole numbers");
+
+        short num = 30899;
+        System.out.println(num + " <- Short, a data type that stores a specific range of values");
+
+    }
+}
